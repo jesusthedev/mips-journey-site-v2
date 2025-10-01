e3d6903c053523aae51e2b46e9af77e02b2a334a
Jesus Ruiz <callmejeebus@gmail.com>
2025-10-01 15:01:21 -0500

    fix: stable JSX-free motion shim

--- src/shims/motion.js
+++ src/shims/motion.js
@@ -1,14 +1,16 @@
 // src/shims/motion.js
-import React from 'react';
+import * as React from 'react';
 
 export const motion = new Proxy({}, {
-  get: (_, tag) => (props = {}) => {
-    const Tag = tag || 'div';
-    const { children, ...rest } = props;
-    return React.createElement(Tag, rest, children);
+  get: (_, tag) => {
+    return function MotionShim(props = {}) {
+      const Tag = tag || 'div';
+      const { children, ...rest } = props;
+      return React.createElement(Tag, rest, children);
+    };
   }
 });
 
-export const AnimatePresence = ({ children }) =>
-  React.createElement(React.Fragment, null, children);
-
+export const AnimatePresence = ({ children }) => {
+  return React.createElement(React.Fragment, null, children);
+};
