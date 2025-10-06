8cae03198233305c0981fa8f6bfcf3db5b8fe93f
Jesus Ruiz <callmejeebus@gmail.com>
2025-10-06 14:39:22 -0500

    feat: add confetti module and styles (fix CI path)

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'   
 import './styles/global.css'
+import './styles/confetti.css';
 import ErrorBoundary from './ErrorBoundary.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><ErrorBoundary><App /></ErrorBoundary></React.StrictMode>)
